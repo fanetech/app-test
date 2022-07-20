006fe6ad99126f3c80f43c6efad1d1b6614c5a17
FIRST_NAME LAST_NAME <louguefranck.20@gmail.com>
2022-07-20 17:06:18 +0000

    coding cors config

--- server.js
+++ server.js
@@ -4,18 +4,29 @@ require("dotenv").config({ path: "./config/.env" });
 const userRoutes = require("./routes/user.routes");
 const itemRoutes = require("./routes/item.routes");
 const authMiddleware = require("./middlewares/auth.middleware");
+const cors = require("cors");
 
 require("./config/db");
 
 const app = express();
 const PORT = process.env.PORT ?? 5000;
 
+//cors
+const corsOptions = {
+  url: process.env.CLIENT_URL,
+  origin: true,
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 //mildelware
 //jwt check all get request if jwt token is exist
-app.get("*", authMiddleware.checkUser);
+// app.get("*", authMiddleware.checkUser);
 
 //routes
 app.use("/api/user", userRoutes);
